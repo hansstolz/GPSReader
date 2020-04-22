743e399c7f648b3da8780aff9cf388fd3b3bd2f3
go <hans@stolz.io>
2020-04-22 21:04:25 +0200

    System

---
+++ Sources/GetExif/shim.h
@@ -0,0 +1,4 @@
+#include "/usr/local/include/libexif/exif-tag.h"
+#include "/usr/local/include/libexif/exif-data.h"
+#include "/usr/local/include/libexif/exif-utils.h"
+#include "/usr/local/include/libexif/exif-ifd.h"
